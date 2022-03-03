1e77aa5150607f56413f264a7fc1772f0a5b6c89
Deena Smith <deenasmith1630@gmail.com>
2022-03-03 10:14:14 -0600

    HTML skeleton and JS pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Start button--on click the timer starts and the first question is presented
+
+
+// After answer is selected, the next question is presented--Loop through questions until the end or timer is out
+
+
+// If/else statement--is answer is incorrect, time is deducted from timer
+
+
+// If/else statement--when loop is done the game is complete
+
+
+// Use local storage to take in initials and high score after game ends
