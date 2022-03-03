61dc70adf620d539dc0c5c228428a05d9e4afe2f
Deena Smith <deenasmith1630@gmail.com>
2022-03-03 12:13:39 -0600

    JS variables built, answer buttons added, started object for questions

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 
+// Variables
+var initiateQuiz = document.querySelector('#start-button');
+var beginTimer = document.querySelector('#timer');
+var promptQuestions = document.querySelector('#questions');
+var answerOptions = document.querySelector('#answers');
+var userInitials = document.querySelector('#enter-initials');
+var answerBtn = document.querySelector('.answer-button');
+
+// Object of questions and answers
+const quiz = {
+    q1: {
+        question: "What is the name of the model CSS uses for its basic structure?",
+        option1: "Cube Model",
+        option2: "Square Structure",
+        answer: "Box Model"
+    },
+    q2: {
+        question: "What does DOM stand for?",
+        option1: "Direct Orientation Mode",
+        answer: "Document Object Model",
+        option2: ""
+    }
+}
+console.log(quiz.q1.answer);
+
 // Start button--on click the timer starts and the first question is presented
 
+initiateQuiz.addEventListener('click', beginQuiz);
+
+
+function beginQuiz() {
+    console.log(beginTimer + 'clicked');
+}; 
+
+
+// Cycle through questions
+promptQuestions.addEventListener('click', answerQuestion);
+
+function answerQuestion() {
+
+}
 
 // After answer is selected, the next question is presented--Loop through questions until the end or timer is out
 
