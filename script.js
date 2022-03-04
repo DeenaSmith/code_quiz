5d3bdd6f3d2f86cd3dcf596aacb87eeb86ac914a
Deena Smith <deenasmith1630@gmail.com>
2022-03-04 15:18:52 -0600

    timer works, begins when start button clicked

--- script.js
+++ script.js
@@ -34,31 +34,48 @@ const quiz = {
     }
 }
 
-// Variables
-var initiateQuiz = document.querySelector('#start-button');
+
+// Timer Variables 
 var beginTimer = document.querySelector('#timer');
+var startingSeconds = 60;
+
+
+// Quiz Variables
+var initiateQuiz = document.querySelector('#start-button');
 var promptQuestion = document.querySelector('#questions');
 var userInitials = document.querySelector('#enter-initials');
 var answerBtn1 = document.querySelector('#option1');
 var answerBtn2 = document.querySelector('#option2');
 var answerBtn3 = document.querySelector('#option3');
 var optionsBtn = document.getElementsByClassName('.options')
+var getResults = document.querySelector('#results');
 
 // Start button--on click the timer starts and the first question is presented
-initiateQuiz.addEventListener('click', beginQuiz);
+initiateQuiz.addEventListener('click', beginQuiz, setInterval);
+
 
 
 
 function beginQuiz() {
+    setInterval(timerStart, 1000);
+
+    function timerStart() {
+        var seconds = startingSeconds % 1000;
+
+        beginTimer.innerHTML = `${seconds}`;
+        startingSeconds--;
+        startingSeconds = startingSeconds < 0 ? 0:startingSeconds;
+    };
+
     // display question
     promptQuestion.innerHTML = quiz.q1.question;
 
     // display questions
     answerBtn1.innerHTML = quiz.q1.option1
-    answerBtn1.value = 'false';
+    
 
     answerBtn2.innerHTML = quiz.q1.option2;
-    answerBtn2.value = 'false';
+    
 
     answerBtn3.innerHTML = quiz.q1.answer;
     answerBtn3.value = 'true';
@@ -68,6 +85,13 @@ function beginQuiz() {
         indBtn.addEventListener('click', event => {
     
             var usersAnswer = event.target.value
+
+            if(usersAnswer === true) {
+                getResults.innerHTML = +5;
+            } else {
+
+            }
+            console.log(getResults)
             console.log('userAnswer', usersAnswer)
             
             answerQuestion2();
@@ -82,13 +106,13 @@ function answerQuestion2() {
     promptQuestion.innerHTML = quiz.q2.question;
 
     answerBtn1.innerHTML = quiz.q2.option1;
-    answerBtn1.value = 'false';
+    
 
     answerBtn2.innerHTML = quiz.q2.answer;
     answerBtn2.value = 'true';
 
     answerBtn3.innerHTML = quiz.q2.option2;
-    answerBtn3.value = 'false';
+    
 
 
     document.querySelectorAll('.options').forEach(indBtn => {
@@ -110,10 +134,10 @@ function answerQuestion3() {
     promptQuestion.innerHTML = quiz.q3.question;
 
     answerBtn1.innerHTML = quiz.q3.option1;
-    answerBtn1.value = 'false';
+    
 
     answerBtn2.innerHTML = quiz.q3.option2;
-    answerBtn2.value = 'false';
+    
 
     answerBtn3.innerHTML = quiz.q3.answer;
     answerBtn3.value = 'true';
@@ -142,10 +166,10 @@ function answerQuestion4() {
     answerBtn1.value = 'true';
 
     answerBtn2.innerHTML = quiz.q4.option1;
-    answerBtn2.value = 'false';
+    
 
     answerBtn3.innerHTML = quiz.q4.option2;
-    answerBtn3.value = 'false';
+    
 
 
     document.querySelectorAll('.options').forEach(indBtn => {
@@ -167,13 +191,13 @@ function answerQuestion5() {
     promptQuestion.innerHTML = quiz.q5.question;
 
     answerBtn1.innerHTML = quiz.q5.option1
-    answerBtn1.value = 'false';
+    
 
     answerBtn2.innerHTML = quiz.q5.answer;
     answerBtn2.value = 'true';
 
     answerBtn3.innerHTML = quiz.q5.option2;
-    answerBtn3.value = 'false';
+    
 
 
     document.querySelectorAll('.options').forEach(indBtn => {
