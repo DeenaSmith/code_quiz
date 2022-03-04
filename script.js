6bf215aaa8814cfcfa26c5467486ad72667e381b
Deena Smith <deenasmith1630@gmail.com>
2022-03-03 19:56:16 -0600

    begin question functions

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-// Variables
-var initiateQuiz = document.querySelector('#start-button');
-var beginTimer = document.querySelector('#timer');
-var promptQuestions = document.querySelector('#questions');
-var answerOptions = document.querySelector('#answers');
-var userInitials = document.querySelector('#enter-initials');
-var answerBtn = document.querySelector('.answer-button');
 
 // Object of questions and answers
 const quiz = {
@@ -33,30 +26,68 @@ const quiz = {
         option1: "Math.random()",
         option2: "Math.ceiling()"
     },
-    q5:{
+    q5: {
         question: "In the array var groceries = ['apples', 'grapes', 'bread'], which item is set to index 1?",
         option1: "apples",
         answer: "grapes",
         option2: "groceries"
     }
 }
-console.log(quiz.q1.answer);
+
+// Variables
+var initiateQuiz = document.querySelector('#start-button');
+var beginTimer = document.querySelector('#timer');
+var promptQuestion = document.querySelector('#questions');
+var userInitials = document.querySelector('#enter-initials');
+var answerBtn1 = document.querySelector('#option1');
+var answerBtn2 = document.querySelector('#option2');
+var answerBtn3 = document.querySelector('#option3');
+var optionsBtn = document.querySelector('.options')
 
 // Start button--on click the timer starts and the first question is presented
-
 initiateQuiz.addEventListener('click', beginQuiz);
 
-
 function beginQuiz() {
-    console.log(beginTimer + 'clicked');
+    // display question
+    promptQuestion.innerHTML = quiz.q1.question;
+
+    // display questions
+    answerBtn1.innerHTML = quiz.q1.option1
+    answerBtn1.value = 'false';
+
+    answerBtn2.innerHTML = quiz.q1.option2;
+    answerBtn2.value = 'false';
+
+    answerBtn3.innerHTML = quiz.q1.answer;
+    answerBtn3.value = 'true';
+
+
+    
+    
+    //  document.querySelector('#option1').addEventListener('click', answerQuestion2);
+    //  document.querySelector('#option2').addEventListener('click', answerQuestion2);
+    //  document.querySelector('#option3').addEventListener('click', answerQuestion2);
 }; 
 
+optionsBtn.addEventListener('click', function (event) {
+    var usersAnswer = event.target.value
+    console.log('userAnswer', usersAnswer)
+
+    if(usersAnswer.includes('true')){
+        answerQuestion2();
+    }else if(usersAnswer.includes('false')){
+        console.log('nope')
+     
+    }
+});
 
 // Cycle through questions
-promptQuestions.addEventListener('click', answerQuestion);
 
-function answerQuestion() {
-
+function answerQuestion2() {
+    promptQuestion.innerHTML = quiz.q2.question;
+    answerBtn1.innerHTML = quiz.q2.option1;
+    answerBtn2.innerHTML = quiz.q2.answer;
+    answerBtn3.innerHTML = quiz.q2.option2;
 }
 
 // After answer is selected, the next question is presented--Loop through questions until the end or timer is out
